87434db544a2d908c640992a3b4beb0727ec5d7e
NishantKSinha <nishantsinha233@gmail.com>
2025-08-15 22:27:23 +0530

    againcommit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.dev/confi/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Ecommerce-Website"
+  base: process.env.VITE_BASE_PATH || "/Ecommerce-Website",
 })
