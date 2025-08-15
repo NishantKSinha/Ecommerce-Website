5394daccb3293ef530d0a75a1913dbf15904be66
NishantKSinha <nishantsinha233@gmail.com>
2025-08-15 22:37:02 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/confi/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Ecommerce-Website",
+  
 })
